138fbe181573e35a791a50d180a90f44c7bebdc2
DonCosme <109149406+franema@users.noreply.github.com>
2022-12-05 19:01:21 -0300

    Allows to add a product to the cart

--- src/Pages/Shop.js
+++ src/Pages/Shop.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
+import { products } from '../Products/AllProducts'
 
-function Shop() {
+function Shop( { addProduct } ) {
+
+
   return (
     <div>
       <Navbar></Navbar>
-      <h1>SHOP</h1>
+      <div className='shop-container'>
+        {products.map(product => (<div key={product} onClick={(e) => addProduct(e.target)} className='product'> {product} </div> ))}
+      </div>
     </div>
   )
 }
