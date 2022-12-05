f206cfed31f76ef2e2922c4993fd65e47e44bd46
DonCosme <109149406+franema@users.noreply.github.com>
2022-12-05 15:06:09 -0300

    Create About.js

---
+++ src/Pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../Components/Navbar'
+
+
+function About() {
+  return (
+    <div>
+        <Navbar></Navbar>
+        <h1>ABOUT</h1>
+    </div>
+  )
+}
+
+export default About
