addcfd0170d9a31e522d3c26fde5692dcfb5f450
DonCosme <109149406+franema@users.noreply.github.com>
2022-12-05 15:06:07 -0300

    Create Cart.js

---
+++ src/Pages/Cart.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../Components/Navbar'
+
+function Cart() {
+  return (
+    <div>
+        <Navbar></Navbar>
+        <h1>Cart</h1>
+    </div>
+  )
+}
+
+export default Cart
