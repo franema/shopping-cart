c90b3d0c1acef88a4e27e380c405399d6866eef5
DonCosme <109149406+franema@users.noreply.github.com>
2022-12-05 15:06:01 -0300

    Create Shop.js

---
+++ src/Pages/Shop.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../Components/Navbar'
+
+function Shop() {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <h1>SHOP</h1>
+    </div>
+  )
+}
+
+export default Shop
