d3d0c1d4047f96aabc8b05fd04eb6793e511cb28
DonCosme <109149406+franema@users.noreply.github.com>
2022-12-05 15:06:04 -0300

    Create Home.js

---
+++ src/Pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../css/home.css"
+import Navbar from '../Components/Navbar'
+
+function Home() {
+  return (
+    <div className='content'>
+        <Navbar></Navbar>
+    </div>
+  )
+}
+
+export default Home
