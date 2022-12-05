107a62b6676345f29330d66668db8194d0328300
DonCosme <109149406+franema@users.noreply.github.com>
2022-12-05 19:01:52 -0300

    Sets links routes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
@@ -5,13 +7,21 @@ import Shop from "./Pages/Shop";
 import Cart from "./Pages/Cart";
 
 function App() {
+
+  const [product, setProduct] = useState([])
+
+  function addProduct (newProduct) {
+    setProduct([...product, newProduct])
+  }
+
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/shop" element={<Shop />} />
+        <Route path="/shop" element={<Shop addProduct={addProduct}/>} />
         <Route path="/about" element={<About/>} />
-        <Route path="/cart" element={<Cart/>}></Route>
+        <Route path="/cart" element={<Cart addedProducts={product}/>}></Route>
       </Routes>
     </BrowserRouter>
   );
