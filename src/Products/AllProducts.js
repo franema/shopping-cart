3497cb2e45a29dac109e7a0d6181982c9d4c3cb2
DonCosme <109149406+franema@users.noreply.github.com>
2022-12-06 10:48:19 -0300

    Creates products object
    Allows to set different attributes to each product, so user can get a product image, price, and choose how many products wants to buy

--- src/Products/AllProducts.js
+++ src/Products/AllProducts.js
@@ -1,3 +1,12 @@
+import draw1 from "../img/draw1.jpg"
+import draw2 from "../img/draw2.jpg"
+import draw3 from "../img/draw3.jpg"
+import draw4 from "../img/draw4.jpg"
+
+
 export const products = [
-    "draw1", "draw2", "draw3", "draw4"
+    {name: "draw1", id: 0, price: 150, img: draw1, quantity: 0}, 
+    {name: "draw2", id: 1, price: 200, img: draw2, quantity: 0}, 
+    {name: "draw3", id: 2, price: 250, img: draw3, quantity: 0}, 
+    {name: "draw4", id: 3, price: 100, img: draw4, quantity: 0}
 ]
