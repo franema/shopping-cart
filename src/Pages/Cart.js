8160a001c88f333515f200c4cb3f303dbf10e7fc
DonCosme <109149406+franema@users.noreply.github.com>
2022-12-06 17:08:46 -0300

    Displays a message if the cart is empty

--- src/Pages/Cart.js
+++ src/Pages/Cart.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-function Cart( {addedProducts} ) {
+function Cart({ addedProducts }) {
+
+  const [emptyCart, setEmptyCart] = useState(true)
+
+  useEffect(() => {
+    setEmptyCart(addedProducts.length === 0)
+  }, [addedProducts])
 
   return (
-    <div>
-        <h1>Cart</h1>
-        {addedProducts.map(product => (<div key={product.id}>{product.name}, {product.quantity}</div>))}
+    <div className='cart'>
+      {
+        emptyCart ? (
+          <h1>The cart is empty</h1>
+        ) : (
+          <div>
+            <h1>Cart</h1>
+            {addedProducts.map(product => (<div key={product.id}>{product.name}, {product.quantity}</div>))}
+          </div>
+        )
+      }
     </div>
   )
 }
