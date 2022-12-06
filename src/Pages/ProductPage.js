1f57e2cb8f52ddad336c7897055da8c77f432b37
DonCosme <109149406+franema@users.noreply.github.com>
2022-12-06 17:09:27 -0300

    Allows to set quantity of products
    User can select how many items wants to add to the cart

--- src/Pages/ProductPage.js
+++ src/Pages/ProductPage.js
@@ -2,27 +2,45 @@ import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { products } from '../Products/AllProducts'
 
-function ProductPage({addProduct}) {
+function ProductPage({ addProduct, cartProducts }) {
 
     let linkedProduct = useParams()
-    const [selectedProduct, setSelectedProducts] = useState(products[linkedProduct.id])
+    const [selectedProduct, setSelectedProducts] = useState(0)
+    const [quantity, setQuantity] = useState(0)
 
     useEffect(() => {
         setSelectedProducts(products[linkedProduct.id])
     }, [selectedProduct, linkedProduct])
 
+    useEffect (() => {
+        cartProducts.map(product => {
+            if(product.id === selectedProduct.id) {
+                setQuantity(product.quantity)
+            }
+        })
+    }, [cartProducts, selectedProduct])
+
+    function increaseQuantity () {
+        setQuantity(quantity + 1)
+    }
+
+    function decreaseQuantity () {
+        setQuantity(quantity - 1)
+    }
+
     return (
         <div className='selected-product-container'>
             <div className='selected-product'>
-                <h1>{selectedProduct.name}</h1>
-                <p>Price: ${selectedProduct.price}</p>
-                <p>{selectedProduct.img}</p>
+                <img src={selectedProduct.img} alt={selectedProduct.name}></img>
             </div>
             <div className='add-to-cart'>
                 <h1>{selectedProduct.name}</h1>
-                <p>{selectedProduct.img}</p>
-                <p>{selectedProduct.quantity}</p>
-                <button onClick={() => {addProduct(selectedProduct)}}>Add to Cart</button>
+                <div>
+                    <button onClick={decreaseQuantity}>-</button>
+                    <input type="number" disabled={true} value={quantity}></input>
+                    <button onClick={increaseQuantity}>+</button>
+                </div>
+                <button onClick={() => addProduct(selectedProduct, quantity)}>Add to cart</button>
             </div>
         </div>
     )
