defd2e52f29dd6dd14ac57337eb5d96153e1a53a
DonCosme <109149406+franema@users.noreply.github.com>
2022-12-06 10:50:39 -0300

    Cleans code

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import "../css/home.css"
-import Navbar from '../Components/Navbar'
 
 function Home() {
   return (
     <div className='content'>
-        <Navbar></Navbar>
     </div>
   )
 }
