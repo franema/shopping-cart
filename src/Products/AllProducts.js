e0ecc15b657e7bad8787773c499a6a88d2b89c46
DonCosme <109149406+franema@users.noreply.github.com>
2022-12-06 17:09:40 -0300

    Rename products

--- src/Products/AllProducts.js
+++ src/Products/AllProducts.js
@@ -5,8 +5,8 @@ import draw4 from "../img/draw4.jpg"
 
 
 export const products = [
-    {name: "draw1", id: 0, price: 150, img: draw1, quantity: 0}, 
-    {name: "draw2", id: 1, price: 200, img: draw2, quantity: 0}, 
-    {name: "draw3", id: 2, price: 250, img: draw3, quantity: 0}, 
-    {name: "draw4", id: 3, price: 100, img: draw4, quantity: 0}
+    {name: "Draw 1", id: 0, price: 150, img: draw1, quantity: 0}, 
+    {name: "Draw 2", id: 1, price: 200, img: draw2, quantity: 0}, 
+    {name: "Draw 3", id: 2, price: 250, img: draw3, quantity: 0}, 
+    {name: "Draw 4", id: 3, price: 100, img: draw4, quantity: 0}
 ]
