bde5ce26200c856526fbc6cd9f427a5fc78d3693
DonCosme <109149406+franema@users.noreply.github.com>
2022-12-06 10:49:34 -0300

    Allows to add a product to the cart
    User can incorporate products to the cart, and choose quantity

--- src/App.js
+++ src/App.js
@@ -1,27 +1,38 @@
 
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
 import Shop from "./Pages/Shop";
 import Cart from "./Pages/Cart";
+import Navbar from "./Components/Navbar";
+import ProductPage from "./Pages/ProductPage";
+import { products } from "./Products/AllProducts";
 
 function App() {
 
-  const [product, setProduct] = useState([])
+  const [cartProducts, setCartProducts] = useState([])
 
-  function addProduct (newProduct) {
-    setProduct([...product, newProduct])
+  function addProduct(newProduct) {
+    newProduct.quantity += 1
+    for(let i = 0; i<cartProducts.length; i++) {
+      if(cartProducts[i].id === newProduct.id) {
+        setCartProducts([...cartProducts])
+        return
+      } 
+    }
+    setCartProducts([...cartProducts, newProduct])
   }
-
 
   return (
     <BrowserRouter>
+      <Navbar></Navbar>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/shop" element={<Shop addProduct={addProduct}/>} />
-        <Route path="/about" element={<About/>} />
-        <Route path="/cart" element={<Cart addedProducts={product}/>}></Route>
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/shop/:id" element={<ProductPage addProduct={addProduct}/>} />
+        <Route path="/about" element={<About />} />
+        <Route path="/cart" element={<Cart addedProducts={cartProducts} />}></Route>
       </Routes>
     </BrowserRouter>
   );
