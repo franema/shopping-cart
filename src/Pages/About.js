589dacb59106b618c1453b7eba3b298bf7e95e5e
DonCosme <109149406+franema@users.noreply.github.com>
2022-12-06 10:46:17 -0300

    Updates img folder

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import Navbar from '../Components/Navbar'
 
 
 function About() {
   return (
     <div>
-        <Navbar></Navbar>
         <h1>ABOUT</h1>
     </div>
   )
