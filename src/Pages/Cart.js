6d8305ba727cadcc497498cbde7a11a095039129
DonCosme <109149406+franema@users.noreply.github.com>
2022-12-07 11:28:35 -0300

    Rearrange cart products display
    Shows image, price, quantity, subtotal and total amount of the cart

--- src/Pages/Cart.js
+++ src/Pages/Cart.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 
 function Cart({ addedProducts }) {
 
@@ -14,9 +15,23 @@ function Cart({ addedProducts }) {
         emptyCart ? (
           <h1>The cart is empty</h1>
         ) : (
-          <div>
+          <div className='cart-product-container'>
             <h1>Cart</h1>
-            {addedProducts.map(product => (<div key={product.id}>{product.name}, {product.quantity}</div>))}
+            {addedProducts.map(product => (
+              <div className='cart-product' key={product.id}>
+                <p>{product.name}</p>
+                <Link to={`/shop/${product.id}`}><img className='cart-product-img' src={product.img} alt={product.name}></img> </Link>
+                <p>Quantity: {product.quantity}</p>
+                <p className='price'>Subtotal ${product.quantity * product.price}</p>
+                <button>Delete</button>
+              </div>))}
+            {addedProducts.length > 1 ? (
+              <p>Total: ${addedProducts.reduce((acc, obj) => acc + (obj.quantity * obj.price), 0
+              )}</p>
+            ) : (
+              <p>Total: ${addedProducts[0].quantity * addedProducts[0].price}</p>
+            )
+            }
           </div>
         )
       }
