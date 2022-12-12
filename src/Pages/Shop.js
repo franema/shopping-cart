9dc4afd2f8879f8aaedd78b22384ccc778e31104
DonCosme <109149406+franema@users.noreply.github.com>
2022-12-12 15:19:17 -0300

    Update Shop.js
    Testing loader in shop page

--- src/Pages/Shop.js
+++ src/Pages/Shop.js
@@ -1,19 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { products } from '../Products/AllProducts'
 import { Link } from 'react-router-dom'
+import Loader from '../Components/Loader'
+
 
 function Shop() {
 
+  const [loaderStyle, setloaderStyle] = useState({
+    visibility: "visible",
+  })
+
+  function removeLoader () {
+    setloaderStyle ({visibility: "hidden"})
+  }
 
   return (
     <div>
-      <div className='shop-container'>
+      <Loader loaderStyle={loaderStyle}></Loader>
+      <div className='shop-container' onLoad={removeLoader}>
         {products.map(product => (
           <Link to={`/shop/${product.id}`} key={product.id} className='product'>
 
             <p>{product.name}</p>
             <p>${product.price}</p>
-            <img className='product-image' alt={product.name} src={product.img}></img>
+            <img className='product-image' alt={product.name} src={product.img} ></img>
 
           </Link>))}
       </div>
