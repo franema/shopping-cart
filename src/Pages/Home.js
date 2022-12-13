5591a51d42485f6eeef63394b4ed2326932226a4
DonCosme <109149406+franema@users.noreply.github.com>
2022-12-13 08:52:33 -0300

    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import "../css/home.css"
+import artist from "../img/home-background.jpg"
 
 function Home() {
+
+
+
   return (
     <div className='content'>
+      <h1>Art made by real Artist!</h1>
+      <div className='artist_container'>
+        <img className='artist_image' src={artist}></img>
+      </div>
     </div>
   )
 }
